af8570fd39ce14153ab6a423473f5d373159447c
sulcm <sulcmatej@seznam.cz>
2023-10-17 11:26:13 +0200

    still overfitting

--- run_scripts/configs/wav2vec2/finetune_speech_recognition_ctc_test.sh
+++ run_scripts/configs/wav2vec2/finetune_speech_recognition_ctc_test.sh
@@ -6,26 +6,28 @@ python run_speech_recognition_ctc.py \
 	--dataset_config_name="cs" \
 	--text_column_name="sentence" \
 	--audio_column_name="audio" \
-	--train_split_name="validation" \
-	--eval_split_name="test" \
-	--eval_metrics="wer" \
+	--train_split_name="test" \
+	--eval_split_name="validation" \
+	--eval_metrics wer cer \
 	--evaluation_strategy="steps" \
 	--max_steps="10000" \
-	--learning_rate="1e-4" \
-	--weight_decay="0.005" \
-	--warmup_steps="1000" \
+	--learning_rate="5e-4" \
+	--warmup_steps="500" \
 	--layerdrop="0.0" \
-	--chars_to_ignore , ? . ! - — – \; \: \" “ % ‘ „ ” � … \
+	--mask_time_prob="0.3" \
+	--mask_time_length="10" \
+	--mask_feature_prob="0.1" \
+	--mask_feature_length="64" \
+	--chars_to_ignore , ? . ! - \; \: \" “ % ‘ ” � \
 	--freeze_feature_encoder \
 	--fp16 \
 	--group_by_length \
-	--length_column_name="input_length" \
 	--do_train \
 	--do_eval \
 	--load_best_model_at_end=True \
 	--metric_for_best_model="wer" \
 	--greater_is_better=False \
-	--gradient_accumulation_steps="2" \
+	--gradient_accumulation_steps="1" \
 	--per_device_train_batch_size="8" \
 	--save_steps="500" \
 	--eval_steps="500" \
