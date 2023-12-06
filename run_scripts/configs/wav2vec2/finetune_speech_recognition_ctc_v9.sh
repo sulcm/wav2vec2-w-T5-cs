61abe76aa47852839473909440c4946b5487c1de
sulcm <sulcmatej@seznam.cz>
2023-12-06 14:16:41 +0100

    fixed batch size and other params

--- run_scripts/configs/wav2vec2/finetune_speech_recognition_ctc_v9.sh
+++ run_scripts/configs/wav2vec2/finetune_speech_recognition_ctc_v9.sh
@@ -11,15 +11,14 @@ python run_speech_recognition_ctc.py \
 	--eval_metrics wer cer \
 	--evaluation_strategy="steps" \
 	--max_steps="40000" \
-	--learning_rate="3e-5" \
+	--learning_rate="5e-5" \
 	--warmup_steps="4000" \
-	--layerdrop="0.075" \
-    --activation_dropout="0.1" \
+	--layerdrop="0.1" \
 	--chars_to_ignore , ? . ! - \; \: \" “ % ‘ ” � \
 	--freeze_feature_encoder \
-	--mask_time_prob="0.65" \
+	--mask_time_prob="0.065" \
 	--mask_time_length="10" \
-	--mask_feature_prob="0.25" \
+	--mask_feature_prob="0.008" \
 	--mask_feature_length="64" \
 	--fp16 \
 	--group_by_length \
@@ -28,11 +27,12 @@ python run_speech_recognition_ctc.py \
 	--load_best_model_at_end=True \
 	--metric_for_best_model="wer" \
 	--greater_is_better=False \
-	--gradient_accumulation_steps="8" \
-	--per_device_train_batch_size="64" \
-	--save_steps="1000" \
-	--eval_steps="1000" \
-	--logging_steps="10" \
+	--gradient_accumulation_steps="4" \
+	--per_device_train_batch_size="128" \
+	--save_steps="2000" \
+	--eval_steps="2000" \
+	--logging_steps="100" \
 	--save_total_limit="2" \
 	--gradient_checkpointing \
-	--dataloader_num_workers="4"
+	--dataloader_num_workers="4" \
+	--preprocessing_num_workers="4"
