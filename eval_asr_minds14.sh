1c093a1c881be8795991940801a4b9970bd4bc06
sulcm <sulcmatej@seznam.cz>
2023-12-28 18:12:28 +0100

    dataset analysis and result formating

--- eval_asr_minds14.sh
+++ eval_asr_minds14.sh
@@ -1,6 +1,6 @@
 python eval.py \
 	--model_name_or_path="/home/sulcm/models/wav2vec2/wav2vec2-cs-$1" \
-    --output_dir="/home/sulcm/models/wav2vec2/eval_wav2vec2/wav2vec2-cs-$1/" \
+    --output_dir="./results/wav2vec2/wav2vec2-cs-$1/" \
 	--dataset_name="PolyAI/minds14" \
 	--dataset_config_name="cs-CZ" \
 	--text_column_name="transcription" \
