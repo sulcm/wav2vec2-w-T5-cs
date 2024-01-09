c15f8dd0483792ddab3c41c43514f5d1c0c20ed5
sulcm <sulcmatej@seznam.cz>
2024-01-09 16:10:33 +0100

    just diff structure

--- eval_scripts/common_voice_11.sh
+++ eval_scripts/common_voice_11.sh
@@ -1,10 +1,10 @@
-python eval.py \
+python model_eval.py \
 	--model_name_or_path="/home/sulcm/models/wav2vec2/wav2vec2-cs-$1" \
     --output_dir="./results/wav2vec2/wav2vec2-cs-$1/" \
-	--dataset_name="PolyAI/minds14" \
-	--dataset_config_name="cs-CZ" \
-	--text_column_name="transcription" \
+	--dataset_name="mozilla-foundation/common_voice_11_0" \
+	--dataset_config_name="cs" \
+	--text_column_name="sentence" \
 	--audio_column_name="audio" \
-	--test_split_name="train" \
+	--test_split_name="test" \
 	--metrics "wer cer" \
 	--chars_to_ignore ", ? . ! - \; \: \" “ % ‘ ” �"
