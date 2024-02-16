a75031b593289644e7f7aa6e858d5d651a74f5bb
sulcm <sulcmatej@seznam.cz>
2024-02-16 18:48:04 +0100

    error eval and new t5 finetuning

--- asr_w_spellchecker.py
+++ asr_w_spellchecker.py
@@ -12,8 +12,8 @@ from transformers import (
 class ST6():
     framework = "pt"
 
-    def __init__(self, wav2vec2_path: str, t5_path: str="", use_cuda: bool=True) -> None:
-        self.logger = self.get_logger(__name__)
+    def __init__(self, wav2vec2_path: str, t5_path: str="", use_cuda: bool=True, logging_level=logging.INFO) -> None:
+        self.logger = self.get_logger(name=__name__, logging_level=logging_level)
 
         self.w2v2_path = wav2vec2_path
         self.t5_path = t5_path
@@ -37,28 +37,28 @@ class ST6():
         self.logger.info("ST6 was successfully initialized")
         pass
 
-    def forward(self, input_audio: list|list[list]) -> list:
-        inputs = self.w2v2_processor(input_audio, sampling_rate=self.sampling_rate, return_tensors=self.framework).to(self.device)
+    def forward(self, input_audio: list|list[list], return_asr_output: bool=False) -> list:
+        inputs = self.w2v2_processor(input_audio, sampling_rate=self.sampling_rate, return_tensors=self.framework, padding=True).to(self.device)
         with torch.no_grad():
             logits = self.w2v2_model(**inputs).logits
         pred_ids = torch.argmax(logits, dim=-1)
-        transcription = self.w2v2_processor.batch_decode(pred_ids)
+        transcription = self.w2v2_processor.batch_decode(pred_ids, skip_special_tokens=True)
 
         if self.t5_path == "":
             return transcription
         
         self.logger.debug(f"Wav2Vec2 transcription: {transcription}")
 
-        inputs = self.t5_tokenizer([self.prefix + sentence for sentence in transcription], return_tensors=self.framework).to(self.device)
+        inputs = self.t5_tokenizer([self.prefix + sentence for sentence in transcription], return_tensors=self.framework, padding=True).to(self.device)
         output_sequences = self.t5_model.generate(**inputs, max_new_tokens=self.max_new_tokens, num_beams=self.num_beams, do_sample=True)
         corrected_input = self.t5_tokenizer.batch_decode(output_sequences, skip_special_tokens=True)
 
-        return corrected_input
+        return corrected_input if not return_asr_output else (corrected_input, transcription)
     
-    def __call__(self, input_audio: list|list[list]) -> list:
-        return self.forward(input_audio=input_audio)
+    def __call__(self, input_audio: list|list[list], return_asr_output: bool=False) -> list:
+        return self.forward(input_audio=input_audio, return_asr_output=return_asr_output)
     
-    def get_logger(self, name: str=__name__, logging_level=logging.INFO) -> logging.Logger:
+    def get_logger(self, name: str=__name__, logging_level=logging.NOTSET) -> logging.Logger:
         logger = logging.getLogger(name)
         logger.setLevel(logging_level)
         formatter = logging.Formatter('%(asctime)s - %(module)s.%(funcName)s - %(levelname)s: %(message)s')
